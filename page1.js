4eebe21b264b6eb8309eee34f618080a2c927104
AS <as69838801@gmail.com>
2025-12-11 12:03:59 +0800

    newBranch

--- page1.js
+++ page1.js
@@ -1,4 +1,12 @@
+let foodArr = [];
+
 // add this function to html of search bar
 function searchFunc() {
     let foodInput = prompt("Type food here or click options below: ");
+    foodArr.push(foodInput);
+};
+
+//add this function to html of food suggestion buttons
+function buttonFunc(btn) { 
+    foodArr.push(btn.textContent); 
 };
